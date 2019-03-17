a9c7afd8dfd5166d6011e548e9349edb7601f93e
Yuriy Grebenyuk <yuriyyyyy@gmail.com>
2019-03-17 22:17:01 +0200

    Landing

---
+++ precache-manifest.c714b97874095c9077d722042d37661e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "66e784b502397fafb28e9d83c4bcede7",
+    "url": "/static/media/sec2Image.66e784b5.jpg"
+  },
+  {
+    "revision": "6be773193d5d32e1cce4354104e3ff42",
+    "url": "/static/media/header_background.6be77319.jpg"
+  },
+  {
+    "revision": "105c9533170d307dea97092d74260608",
+    "url": "/static/media/footer_img6.105c9533.jpg"
+  },
+  {
+    "revision": "2dd26a9f5abc2186c154e3648bdcb287",
+    "url": "/static/media/footer_img4.2dd26a9f.jpg"
+  },
+  {
+    "revision": "da43c1223a59dc2c1d78a8b305b51cbe",
+    "url": "/static/media/footer_img1.da43c122.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "04c1a54ae66a661f2062",
+    "url": "/static/js/main.04c1a54a.chunk.js"
+  },
+  {
+    "revision": "a6ab2e802001837bce29",
+    "url": "/static/js/2.a6ab2e80.chunk.js"
+  },
+  {
+    "revision": "04c1a54ae66a661f2062",
+    "url": "/static/css/main.1d19d56d.chunk.css"
+  },
+  {
+    "revision": "8f9be3edec9d330b1fcbe2c5fb4312d3",
+    "url": "/index.html"
+  }
+];
